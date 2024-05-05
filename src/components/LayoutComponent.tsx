af241a8fe916be15921375dfbe8a21baf6e8664c
wmg20 <aryapratap.maurya20@gmail.com>
2024-05-06 00:47:20 +0530

    feat: react query for data fetching is added

--- src/components/LayoutComponent.tsx
+++ src/components/LayoutComponent.tsx
@@ -1,14 +1,29 @@
+"use client";
+
 import React from "react";
+import { Container } from "@mui/material";
 import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
 import theme from "@/theme/theme";
-import { Container } from "@mui/material";
 
 const LayoutComponent = ({ children }: { children: React.ReactNode }) => {
+  const queryClient = new QueryClient();
+
   return (
     <AppRouterCacheProvider>
       <CssVarsProvider theme={theme}>
-        <Container maxWidth="xl">{children}</Container>
+        <QueryClientProvider client={queryClient}>
+          <Container maxWidth="xl">{children}</Container>
+        </QueryClientProvider>
       </CssVarsProvider>
     </AppRouterCacheProvider>
   );
